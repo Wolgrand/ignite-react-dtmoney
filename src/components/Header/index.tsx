0c5f837c18d1ca2a58587bb00a3441dc92802e3a
Wolgrand Neto <wolgrand@live.com>
2021-03-18 20:31:53 -0300

    Module Finished

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+interface Headerprops {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}:Headerprops) {
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money"/>
+        <button type='button' onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+
+      </Content>
+    </Container>
+  )
+}
